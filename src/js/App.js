124ac1e793748dd8da89747f5cc450abf52c1773
VR314 <raghupathivaroon@gmail.com>
2022-07-27 02:12:21 +0000

    make grid

---
+++ src/js/App.js
@@ -0,0 +1,28 @@
+"use strict";
+let s = "test";
+console.log(s);
+const ROWS = 20;
+const COLUMNS = 20;
+function defineGrid() {
+    let container = document.getElementById("container");
+    if (container != null) {
+        container.style.gridTemplateColumns = `${100 / (COLUMNS)}%`.repeat(COLUMNS);
+        container.style.gridTemplateRows = `${100 / (ROWS)}%`.repeat(ROWS);
+    }
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMNS; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = `${(i) * COLUMNS + j}`;
+            cell.style.border = "solid 0.7px black";
+            cell.addEventListener("mouseenter", (e) => {
+                cell.classList.add("hoverCell");
+            });
+            cell.addEventListener("mouseleave", () => {
+                cell.classList.remove("hoverCell");
+            });
+            container?.appendChild(cell);
+        }
+    }
+}
+defineGrid();
